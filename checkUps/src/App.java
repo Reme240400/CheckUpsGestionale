34cfff19f39dab4f28bd63462ad5a9e517abc3ca
Remedi-Tommaso <t.remedi@campus.uniurb.it>
2023-08-01 19:04:14 +0200

    fix

--- checkUps/src/App.java
+++ checkUps/src/App.java
@@ -1,5 +1,43 @@
+import java.sql.*;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        visualizzaDatiTabellaSocieta();
+    }
+
+    public static void visualizzaDatiTabellaSocieta() {
+        String connectionUrl = "jdbc:postgresql://localhost:5432/checkups_db";
+        String username = "postgres";
+        String password = "postgres";
+
+        try (Connection connection = DriverManager.getConnection(connectionUrl, username, password)) {
+            String query = "SELECT * FROM public.societa";
+
+            try (Statement statement = connection.createStatement();
+                 ResultSet resultSet = statement.executeQuery(query)) {
+
+                while (resultSet.next()) {
+                    int idSocieta = resultSet.getInt("id_societa");
+                    String indirizzo = resultSet.getString("indirizzo");
+                    String localita = resultSet.getString("localita");
+                    String provincia = resultSet.getString("provincia");
+                    long telefono = resultSet.getLong("telefono");
+                    String descrizione = resultSet.getString("descrizione");
+                    String ente = resultSet.getString("ente");
+
+                    System.out.println("ID: " + idSocieta);
+                    System.out.println("Indirizzo: " + indirizzo);
+                    System.out.println("Localita: " + localita);
+                    System.out.println("Provincia: " + provincia);
+                    System.out.println("Telefono: " + telefono);
+                    System.out.println("Descrizione: " + descrizione);
+                    System.out.println("Ente: " + ente);
+                    System.out.println();
+                }
+            }
+
+        } catch (SQLException ex) {
+            System.out.println("Errore di connessione al database: " + ex.getMessage());
+        }
     }
 }
