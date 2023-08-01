52903165db976c8c6271ac31b8a984d935c19314
Remedi-Tommaso <t.remedi@campus.uniurb.it>
2023-08-01 18:05:18 +0200

    Inizio

---
+++ src/main/java/checkups/CheckUpsGestionale/CheckUpsGestionaleApplication.java
@@ -0,0 +1,13 @@
+package checkups.CheckUpsGestionale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckUpsGestionaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckUpsGestionaleApplication.class, args);
+	}
+
+}
