abaaee82fcba9a342e551d00b124ec30f208418c
Montesi-Flavio <61314051+Montesi-Flavio@users.noreply.github.com>
2023-08-01 20:34:51 +0200

    .

--- checkUps/src/myMain.java
+++ checkUps/src/myMain.java
@@ -3,12 +3,11 @@ import sql.ControllerSql;
 
 public class myMain {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
         visualizzaDatiTabellaSocieta();
     }
 
     public static void visualizzaDatiTabellaSocieta() {
         new ControllerSql();
-
     }
+
 }
