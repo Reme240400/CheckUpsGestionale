97cc2379c042c07e5f1d243c2e45eefdccb18f57
Montesi-Flavio <61314051+Montesi-Flavio@users.noreply.github.com>
2023-08-01 19:13:38 +0200

    .

--- checkUps/src/myMain.java
+++ checkUps/src/myMain.java
@@ -1,4 +1,10 @@
-import java.sql.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class myMain {
     public static void main(String[] args) throws Exception {
