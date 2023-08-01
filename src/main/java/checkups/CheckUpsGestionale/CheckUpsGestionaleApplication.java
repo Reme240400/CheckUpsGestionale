0b0d5f96cbc7af5e0ee4dee0f853569274acf212
Remedi-Tommaso <t.remedi@campus.uniurb.it>
2023-08-01 18:25:36 +0200

    commit di prova

--- src/main/java/checkups/CheckUpsGestionale/CheckUpsGestionaleApplication.java
+++ src/main/java/checkups/CheckUpsGestionale/CheckUpsGestionaleApplication.java
@@ -1,5 +1,7 @@
 package checkups.CheckUpsGestionale;
 
+import java.io.Console;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CheckUpsGestionaleApplication {
 
 	public static void main(String[] args) {
+		System.out.println("HELLO WORLD");
 		SpringApplication.run(CheckUpsGestionaleApplication.class, args);
 	}
 
