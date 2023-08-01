39c6c844c1efee3de47d1f303fce3b880586263e
Montesi-Flavio <61314051+Montesi-Flavio@users.noreply.github.com>
2023-08-01 19:36:52 +0200

    .

--- checkUps/src/myMain.java
+++ checkUps/src/myMain.java
@@ -1,10 +1,5 @@
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
+import sql.ControllerSql;
 
 public class myMain {
     public static void main(String[] args) throws Exception {
@@ -13,38 +8,7 @@ public class myMain {
     }
 
     public static void visualizzaDatiTabellaSocieta() {
-        String connectionUrl = "jdbc:postgresql://localhost:5432/checkups_db";
-        String username = "postgres";
-        String password = "1234";
+        new ControllerSql();
 
-        try (Connection connection = DriverManager.getConnection(connectionUrl, username, password)) {
-            String query = "SELECT * FROM public.societa";
-
-            try (Statement statement = connection.createStatement();
-                ResultSet resultSet = statement.executeQuery(query)) {
-
-                while (resultSet.next()) {
-                    int idSocieta = resultSet.getInt("id_societa");
-                    String indirizzo = resultSet.getString("indirizzo");
-                    String localita = resultSet.getString("localita");
-                    String provincia = resultSet.getString("provincia");
-                    long telefono = resultSet.getLong("telefono");
-                    String descrizione = resultSet.getString("descrizione");
-                    String ente = resultSet.getString("ente");
-
-                    System.out.println("ID: " + idSocieta);
-                    System.out.println("Indirizzo: " + indirizzo);
-                    System.out.println("Localita: " + localita);
-                    System.out.println("Provincia: " + provincia);
-                    System.out.println("Telefono: " + telefono);
-                    System.out.println("Descrizione: " + descrizione);
-                    System.out.println("Ente: " + ente);
-                    System.out.println();
-                }
-            }
-
-        } catch (SQLException ex) {
-            System.out.println("Errore di connessione al database: " + ex.getMessage());
-        }
     }
 }
