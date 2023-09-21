5cb4aaba281486a07ea0daf89bcc791b01f84977
Montesi-Flavio <61314051+Montesi-Flavio@users.noreply.github.com>
2023-09-21 20:31:04 +0200

    funziona

--- checkUps/src/main/java/myMain.java
+++ checkUps/src/main/java/myMain.java
@@ -1,51 +1,31 @@
 package main.java;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import sql.ControllerSql;
 
 public class myMain extends Application{
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         launch(args);
-        visualizzaDatiTabellaSocieta();
+        //visualizzaDatiTabellaSocieta();
     }
 
-    public static void visualizzaDatiTabellaSocieta() {
-        new ControllerSql();
-    }
+    // public static void visualizzaDatiTabellaSocieta() {
+    //     new ControllerSql();
+    // }
 
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent arg0) {
-                System.out.println("Hello World!");
-            }  
-
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-
-        /*
-        Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
-        Scene scene = new Scene(root);
-        */
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-                primaryStage.setScene(scene);
-                primaryStage.show();
+    public void start(javafx.stage.Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/View/main.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.show();
     }
+
 
 }
 
