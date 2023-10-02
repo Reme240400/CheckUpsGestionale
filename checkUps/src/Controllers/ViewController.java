07ad6d73c1a8ed45692772085d2f771ad2015ebe
Montesi-Flavio <61314051+Montesi-Flavio@users.noreply.github.com>
2023-10-02 13:15:21 +0200

    .

--- checkUps/src/Controllers/ViewController.java
+++ checkUps/src/Controllers/ViewController.java
@@ -1,52 +1,37 @@
 package Controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Stack;
 
 import javafx.fxml.Initializable;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class ViewController implements Initializable{
 
+    
     @FXML
-    private VBox pnItems = null;
-    @FXML
-    private Button btnOverview;
+    private Button btnQuit;
 
     @FXML
-    private Button btnOrders;
+    private Button btnHome;
 
     @FXML
-    private Button btnCustomers;
+    private StackPane stackPane;
 
-    @FXML
-    private Button btnMenus;
-
-    @FXML
-    private Button btnPackages;
-
-    @FXML
-    private Button btnSettings;
-
-    @FXML
-    private Button btnSignout;
-
-    @FXML
-    private Pane pnlCustomer;
-
-    @FXML
-    private Pane pnlOrders;
-
-    @FXML
-    private Pane pnlOverview;
-
-    @FXML
-    private Pane pnlMenus;
 
 
     @FXML
@@ -56,7 +41,35 @@ public class ViewController implements Initializable{
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
+    }
+
+    public void switchToHome(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        stackPane.getChildren().removeAll();
+        stackPane.getChildren().setAll(root);
+
+    }
+
+    public void switchToOrders(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("nuovo.fxml"));
+        stackPane.getChildren().removeAll();
+        stackPane.getChildren().setAll(root);
+    }
+
+    public void logout(ActionEvent event) throws IOException{
+        System.out.println("Logout");
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+
+        alert.setTitle("Quit");
+        alert.setHeaderText("Stai per uscire!");
+        alert.setContentText("Vuoi salvare il lavoro prima di uscire?");
+
+        if(alert.showAndWait().get().equals("OK")){
+            Stage stage = (Stage) btnQuit.getScene().getWindow();
+            System.out.println("Salvataggio in corso...");
+            stage.close();
+        }
     }
     
 }
--- checkUps/src/myMain.java
+++ checkUps/src/myMain.java
@@ -1,8 +1,12 @@
 
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class myMain extends Application{
@@ -12,11 +16,9 @@ public class myMain extends Application{
     @Override
     public void start(javafx.stage.Stage primaryStage) throws Exception {
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/fxml/home.fxml"));
-
-        Parent root = loader.load();
+        Parent root = FXMLLoader.load(getClass().getResource("/View/fxml/main.fxml"));
         primaryStage.setTitle("CheckUp Gestionale");
-        primaryStage.setScene(new Scene(root));
+        
         primaryStage.initStyle(StageStyle.UNDECORATED);
 
         root.setOnMousePressed(event -> {
@@ -29,7 +31,27 @@ public class myMain extends Application{
             primaryStage.setY(event.getScreenY() - y);
         });
 
+        primaryStage.setScene( new Scene(root));
         primaryStage.show();
+
+        primaryStage.setOnCloseRequest(e -> {
+            e.consume();
+            logout(primaryStage);
+            }
+        );
+    }
+
+    public void logout(Stage stage){
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+
+        alert.setTitle("Quit");
+        alert.setHeaderText("Stai per uscire!");
+        alert.setContentText("Vuoi salvare il lavoro prima di uscire?");
+
+        if(alert.showAndWait().get().equals("OK")){
+            System.out.println("Salvataggio in corso...");
+            stage.close();
+        }
     }
 
     public static void main(String[] args) {
